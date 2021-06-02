2437bc52852e90c05661f0b35c5c851d3fd80b8e
Ahmed-88-Gad <asmeg.gad@gmail.com>
2021-06-02 16:56:26 +0200

    first commit

---
+++ website/app2.js
@@ -0,0 +1,55 @@
+/* Global Variables */
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+//let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// PERSONAL API KEY FOR OPENWEATHER API
+let apiURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey = '&appid=c2ac8cebe9a2e0c7ec663f00467a6445';
+
+
+// GET THE BUTTON
+let generator = document.getElementById('generate');
+
+// GET FEELING TEXT 
+let feel = document.getElementById('feelings').value;
+
+generator.addEventListener('click',run);
+// MAIN FUNCTION
+function run()
+  {
+    // GET ZIP CODE
+    let zip = document.getElementById('zip').value;
+    let fullApiURL = apiURL+zip+apiKey;
+    let serverURL = '/add';
+    console.log(serverURL);
+    if (!zip)
+      {
+        alert('PLEASE ENTER ZIP CODE FIRST')
+      }
+    else
+      {
+        console.log(zip);
+        console.log(fullApiURL);
+        getApiData(fullApiURL)
+        .then(function(data){console.log(data);});
+        //postData(serverURL,);
+        // updateUI();
+      }
+}
+
+async function getApiData(url)
+{
+  fetch(url)
+    .then(function(respons){
+      let data = respons.json();
+      console.log(data);
+      return data;
+    })
+    .catch(function(error) {
+      console.log('Fetch problem: ' + error.message);
+    })
+}
+
+//function postData()
